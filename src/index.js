8bcfedf8b713b2a8a1f26ffb2356d1cb80987e8a
Jan Bogoryja-Zakrzewski <jan.bogoryja@gmail.com>
2021-02-05 09:51:32 +0100

    feat: 🎸 Add canvas and basic file structure

---
+++ src/index.js
@@ -0,0 +1,12 @@
+function doIndexStuff() {
+  console.log("%cHello There", "color: violet");
+  
+  // the things to do
+  messWithCanvas();
+
+  console.log("%cI'm finished, cya", "color: violet");
+}
+
+function messWithCanvas() {
+  let canvasElement = document.getElementById("canvas");
+}
