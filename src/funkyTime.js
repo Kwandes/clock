00b577f9fa180413bacce9dd43164bde2bc85569
Kwandes <bz.jasiek@gmail.com>
2021-05-24 09:45:54 +0200

    Add funky time and basic canvas

---
+++ src/funkyTime.js
@@ -0,0 +1,86 @@
+// write down when the program execution has begun
+const startTime = new Date().getTime();
+
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+
+function funkyTime() {
+  console.log("Let's get funky");
+  console.log("Start time: " + startTime);
+
+  const framesPerSecond = 60;
+  let timeInterval = setInterval(() => {
+    //console.log('Current time: ' + (new Date().getTime() - startTime));
+  }, 1000 / framesPerSecond);
+
+  initCanvas();
+  messWithCanvas();
+}
+
+function initCanvas() {
+  // Validate that the client browser supports WebGL for 3D rendering
+  if (!isWebGLSupported()) {
+    alert(
+      "Your browser does not support required features (WebGL). Please switch to a modern browser or enable webGL" +
+        " (https://support.biodigital.com/hc/en-us/articles/218322977-How-to-turn-on-WebGL-in-my-browser)"
+    );
+    console.error(
+      "Your browser does not support required features (WebGL). Please switch to a modern browser or enable WebGL" +
+        " (https://support.biodigital.com/hc/en-us/articles/218322977-How-to-turn-on-WebGL-in-my-browser)"
+    );
+    console.error("no, Internet Explorer is not a modern browser");
+    return false;
+  }
+
+  function isWebGLSupported() {
+    var canvas = document.createElement("canvas");
+    var gl =
+      canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+  
+    return gl && gl instanceof WebGLRenderingContext;
+  }
+  
+  // Set the initial Canvas values
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  context.save(); // Save the initial state of the canvas
+  context.fillStyle = "lightblue";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+  context.restore(); // Remove the lightblue fill styling
+  // The combo of save-restore is used throughout the app to ensure no accidental styling overlap (old elements applying styling to new ones)
+  return true;
+}
+
+function messWithCanvas() {
+  context.save();
+  // Paint the purple rectangle
+  context.fillStyle = "purple";
+  console.log((canvas.width / 8) * 7);
+  context.shadowColor = "black";
+  context.shadowBlur = 10;
+  context.shadowOffsetX = -2;
+  context.shadowOffsetY = 2;
+  context.fillRect(
+    canvas.width / 8,
+    canvas.height / 8,
+    (canvas.width / 8) * 6,
+    (canvas.height / 8) * 6
+  );
+  context.restore();
+
+  // Paint the yellow rectangle
+  context.fillStyle = "yellow";
+  context.shadowColor = "black";
+  context.shadowBlur = 6;
+  context.shadowOffsetX = -1;
+  context.shadowOffsetY = 1;
+  context.fillRect(
+    canvas.width / 4,
+    canvas.height / 4,
+    canvas.width / 2,
+    canvas.height / 2
+  );
+  context.restore();
+
+  document.addEventListener("click", doClickyStuff);
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 main();
 
+
 function main() {
+  // initialize sliders for changing of colors etc
   initSliders();
+
+  // funky time, which is located in funkyTime.js
+  document.getElementById("testButton").addEventListener('click', funkyTime);
 }
 
 function initSliders() {
